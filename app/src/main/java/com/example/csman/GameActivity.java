a09aff006d4d7c456bab2e0f21393ceb95832bf7
jiawenwang <jiawen5@illinois.edu>
2020-04-27 15:23:33 -0500

    fix textView error1

--- app/src/main/java/com/example/csman/GameActivity.java
+++ app/src/main/java/com/example/csman/GameActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
@@ -30,13 +31,31 @@ public class GameActivity extends AppCompatActivity {
             // if entered letter matches one of the letters in the answer, then the letter will become
             // visible in the answer text box
             // otherwise, one of geoff's head disappears
-        });
 
-        TextView answer = findViewById(R.id.answer);
-        answer.setText("Kaku wa baka");
+            EditText input = findViewById(R.id.playerGuess);
+            String userInput = input.getText().toString();
+
+            String[] wordBank = {"Pineapple", "Alice", "Caroline", "Jake", "Kitetsu"};
+            // temporary wordBank before our library is created
+            TextView answer = findViewById(R.id.answer);
+            // convert user's input into a string
+            //randomly get a string array from our software library.
+            String[] tmp = testMatch(userInput, wordBank[1]);
+            String output = "";
+            for (String str : tmp) {
+                output = output + str;
+            }
+            //String output = tmp.toString();
+            answer.setText(output);
+        });
     }
 
-    /** **/
+    /**
+     *
+     * @param userInput user's answer.
+     * @param answer default answer.
+     * @return the result.
+     */
     public String[] testMatch(String userInput, String answer) {
         int answerLength = answer.length();
         // this string array is made up of one-character strings
@@ -52,13 +71,17 @@ public class GameActivity extends AppCompatActivity {
         for (int answerIndex = 0; answerIndex < answer.length(); answerIndex++) {
             /* if the user's input matches one of the letters in the answer string, then it replaces
             the blank underline */
-            if (input != answer.charAt(answerIndex)) {
-                return initial;
+            if (input == answer.charAt(answerIndex)) {
+                newAnswer[answerIndex] = userInput;
+                return newAnswer;
+                //return initial;
+                //continue;
             }
-            newAnswer[answerIndex] = userInput;
-            System.out.println(newAnswer);
+            //return initial;
+            //newAnswer[answerIndex] = userInput;
+            //System.out.println(newAnswer);
         }
-        System.out.println(newAnswer);
+        //System.out.println(newAnswer);
         return newAnswer;
     }
 }
